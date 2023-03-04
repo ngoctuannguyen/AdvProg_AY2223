590beffbf855248013fbf5e04e4b7ffc87fc9772
ngoctuannguyen <59074969+ngoctuannguyen@users.noreply.github.com>
2023-03-04 17:50:41 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -81,7 +81,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == '1')
+    if (isContinued != ' ')
         result = true;
     return result;
 }
