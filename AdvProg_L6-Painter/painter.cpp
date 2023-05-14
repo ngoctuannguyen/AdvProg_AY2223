089b07a15f464092aa3c0539e9db427a80faa67f
Nguyễn Tuấn Ngọc <59074969+ngoctuannguyen@users.noreply.github.com>
2023-05-14 10:55:18 +0700

    Update painter.cpp

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,9 +9,10 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
-	this->color = color;
-	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b , 0);
+    (this->color) = color;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
+
 
 /***
     Args: numPixel (int): number of pixel for jumping forward
@@ -22,9 +23,9 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
-	float rad = (angle / 180) * M_PI;
-	x += cos(rad) * length;
-	y -= sin(rad) * length;
+    double rad = (angle / 180) * M_PI;
+    x += (int) (cos(rad) * (double) numPixel);
+    y -= (int) (sin(rad) * (double) numPixel);
 }
 
 
@@ -37,9 +38,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
-	float rad = (angle / 180) * M_PI;
-	x -= cos(rad) * length;
-	y += sin(rad) * length;
+    this->jumpForward(-numPixel);
 }
 
 
@@ -48,10 +47,18 @@ void Painter::jumpBackward(int numPixel)
         
     Returns:
         None
-***/       
+***/
+
+void compress(double& angle) {
+    while (angle >= 360) angle -= 360;
+    while (angle < 0) angle += 360;
+}
+
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
+    angle += degree;
+    compress(angle);
 }
 
 
@@ -64,6 +71,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    this->turnLeft(-degree);
 }
 
 /***  
@@ -74,12 +82,12 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color   
-	Uint8 r = rand() % 256;
- 	Uint8 g = rand() % 256;
- 	Uint8 b = rand() % 256;
- 	SDL_Color color = { r, g, b };
- 	setColor(color);
+    // TODO: set random color
+    int r = rand() % 256;
+    int g = rand() % 256;
+    int b = rand() % 256;
+    SDL_Color color = {r, g, b};
+    setColor(color);    
 }
 
 
@@ -178,4 +186,3 @@ void Painter::moveBackward(int numPixel)
 {
     moveForward(-numPixel);
 }
-
